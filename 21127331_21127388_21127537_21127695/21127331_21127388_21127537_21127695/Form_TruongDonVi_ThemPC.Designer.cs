e3c5bd7ae0a7720a335f745c72c27a9ffc4c2d25
Shiphuu <psphu2103@gmail.com>
2024-04-20 18:07:53 +0700

    finised UI TruongDonVi form

---
+++ 21127331_21127388_21127537_21127695/21127331_21127388_21127537_21127695/Form_TruongDonVi_ThemPC.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace _21127331_21127388_21127537_21127695
+{
+    partial class Form_TruongDonVi_ThemPC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_mact_pc = new System.Windows.Forms.TextBox();
+            this.txt_nam_pc = new System.Windows.Forms.TextBox();
+            this.txt_hocki_pc = new System.Windows.Forms.TextBox();
+            this.txt_mahp_pc = new System.Windows.Forms.TextBox();
+            this.lb_mact_pc = new System.Windows.Forms.Label();
+            this.lb_nam_pc = new System.Windows.Forms.Label();
+            this.lb_hk_pc = new System.Windows.Forms.Label();
+            this.lb_mahp_pc = new System.Windows.Forms.Label();
+            this.lb_magv_pc = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.gb_DSKHMO = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lb_phancong = new System.Windows.Forms.Label();
+            this.btn_QuayLaiPC = new System.Windows.Forms.Button();
+            this.btn_ThemPC = new System.Windows.Forms.Button();
+            this.gb_DSKHMO.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_mact_pc
+            // 
+            this.txt_mact_pc.Enabled = false;
+            this.txt_mact_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mact_pc.Location = new System.Drawing.Point(170, 358);
+            this.txt_mact_pc.Name = "txt_mact_pc";
+            this.txt_mact_pc.ReadOnly = true;
+            this.txt_mact_pc.Size = new System.Drawing.Size(213, 27);
+            this.txt_mact_pc.TabIndex = 99;
+            this.txt_mact_pc.TabStop = false;
+            // 
+            // txt_nam_pc
+            // 
+            this.txt_nam_pc.Enabled = false;
+            this.txt_nam_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nam_pc.Location = new System.Drawing.Point(170, 291);
+            this.txt_nam_pc.Name = "txt_nam_pc";
+            this.txt_nam_pc.ReadOnly = true;
+            this.txt_nam_pc.Size = new System.Drawing.Size(213, 27);
+            this.txt_nam_pc.TabIndex = 98;
+            this.txt_nam_pc.TabStop = false;
+            // 
+            // txt_hocki_pc
+            // 
+            this.txt_hocki_pc.Enabled = false;
+            this.txt_hocki_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_hocki_pc.Location = new System.Drawing.Point(170, 226);
+            this.txt_hocki_pc.Name = "txt_hocki_pc";
+            this.txt_hocki_pc.ReadOnly = true;
+            this.txt_hocki_pc.Size = new System.Drawing.Size(213, 27);
+            this.txt_hocki_pc.TabIndex = 97;
+            this.txt_hocki_pc.TabStop = false;
+            // 
+            // txt_mahp_pc
+            // 
+            this.txt_mahp_pc.Enabled = false;
+            this.txt_mahp_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mahp_pc.Location = new System.Drawing.Point(170, 163);
+            this.txt_mahp_pc.Name = "txt_mahp_pc";
+            this.txt_mahp_pc.ReadOnly = true;
+            this.txt_mahp_pc.Size = new System.Drawing.Size(213, 27);
+            this.txt_mahp_pc.TabIndex = 96;
+            this.txt_mahp_pc.TabStop = false;
+            // 
+            // lb_mact_pc
+            // 
+            this.lb_mact_pc.AutoSize = true;
+            this.lb_mact_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_mact_pc.Location = new System.Drawing.Point(32, 361);
+            this.lb_mact_pc.Name = "lb_mact_pc";
+            this.lb_mact_pc.Size = new System.Drawing.Size(118, 20);
+            this.lb_mact_pc.TabIndex = 94;
+            this.lb_mact_pc.Text = "Mã chương trình";
+            // 
+            // lb_nam_pc
+            // 
+            this.lb_nam_pc.AutoSize = true;
+            this.lb_nam_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_nam_pc.Location = new System.Drawing.Point(32, 294);
+            this.lb_nam_pc.Name = "lb_nam_pc";
+            this.lb_nam_pc.Size = new System.Drawing.Size(41, 20);
+            this.lb_nam_pc.TabIndex = 93;
+            this.lb_nam_pc.Text = "Năm";
+            // 
+            // lb_hk_pc
+            // 
+            this.lb_hk_pc.AutoSize = true;
+            this.lb_hk_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_hk_pc.Location = new System.Drawing.Point(32, 229);
+            this.lb_hk_pc.Name = "lb_hk_pc";
+            this.lb_hk_pc.Size = new System.Drawing.Size(51, 20);
+            this.lb_hk_pc.TabIndex = 92;
+            this.lb_hk_pc.Text = "Học kì";
+            // 
+            // lb_mahp_pc
+            // 
+            this.lb_mahp_pc.AutoSize = true;
+            this.lb_mahp_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_mahp_pc.Location = new System.Drawing.Point(32, 163);
+            this.lb_mahp_pc.Name = "lb_mahp_pc";
+            this.lb_mahp_pc.Size = new System.Drawing.Size(95, 20);
+            this.lb_mahp_pc.TabIndex = 91;
+            this.lb_mahp_pc.Text = "Mã học phần";
+            // 
+            // lb_magv_pc
+            // 
+            this.lb_magv_pc.AutoSize = true;
+            this.lb_magv_pc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_magv_pc.Location = new System.Drawing.Point(32, 96);
+            this.lb_magv_pc.Name = "lb_magv_pc";
+            this.lb_magv_pc.Size = new System.Drawing.Size(103, 20);
+            this.lb_magv_pc.TabIndex = 90;
+            this.lb_magv_pc.Text = "Mã giảng viên";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(170, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 26);
+            this.comboBox1.TabIndex = 100;
+            // 
+            // gb_DSKHMO
+            // 
+            this.gb_DSKHMO.Controls.Add(this.dataGridView1);
+            this.gb_DSKHMO.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_DSKHMO.Location = new System.Drawing.Point(413, 83);
+            this.gb_DSKHMO.Name = "gb_DSKHMO";
+            this.gb_DSKHMO.Size = new System.Drawing.Size(375, 315);
+            this.gb_DSKHMO.TabIndex = 101;
+            this.gb_DSKHMO.TabStop = false;
+            this.gb_DSKHMO.Text = "Danh sách khóa học mở chưa được phân công";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(363, 286);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lb_phancong
+            // 
+            this.lb_phancong.AutoSize = true;
+            this.lb_phancong.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_phancong.Location = new System.Drawing.Point(186, 19);
+            this.lb_phancong.Name = "lb_phancong";
+            this.lb_phancong.Size = new System.Drawing.Size(433, 30);
+            this.lb_phancong.TabIndex = 102;
+            this.lb_phancong.Text = "Thêm phân công giảng dạy cho giảng viên";
+            // 
+            // btn_QuayLaiPC
+            // 
+            this.btn_QuayLaiPC.AccessibleDescription = "";
+            this.btn_QuayLaiPC.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_QuayLaiPC.Location = new System.Drawing.Point(391, 431);
+            this.btn_QuayLaiPC.Name = "btn_QuayLaiPC";
+            this.btn_QuayLaiPC.Size = new System.Drawing.Size(126, 50);
+            this.btn_QuayLaiPC.TabIndex = 104;
+            this.btn_QuayLaiPC.Text = "Quay lại";
+            this.btn_QuayLaiPC.UseVisualStyleBackColor = true;
+            this.btn_QuayLaiPC.Click += new System.EventHandler(this.btn_QuayLaiPC_Click);
+            // 
+            // btn_ThemPC
+            // 
+            this.btn_ThemPC.AccessibleDescription = "";
+            this.btn_ThemPC.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ThemPC.Location = new System.Drawing.Point(259, 431);
+            this.btn_ThemPC.Name = "btn_ThemPC";
+            this.btn_ThemPC.Size = new System.Drawing.Size(126, 50);
+            this.btn_ThemPC.TabIndex = 103;
+            this.btn_ThemPC.Text = "Thêm";
+            this.btn_ThemPC.UseVisualStyleBackColor = true;
+            this.btn_ThemPC.Click += new System.EventHandler(this.btn_ThemPC_Click);
+            // 
+            // Form_TruongDonVi_ThemPC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 512);
+            this.Controls.Add(this.btn_QuayLaiPC);
+            this.Controls.Add(this.btn_ThemPC);
+            this.Controls.Add(this.lb_phancong);
+            this.Controls.Add(this.gb_DSKHMO);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txt_mact_pc);
+            this.Controls.Add(this.txt_nam_pc);
+            this.Controls.Add(this.txt_hocki_pc);
+            this.Controls.Add(this.txt_mahp_pc);
+            this.Controls.Add(this.lb_mact_pc);
+            this.Controls.Add(this.lb_nam_pc);
+            this.Controls.Add(this.lb_hk_pc);
+            this.Controls.Add(this.lb_mahp_pc);
+            this.Controls.Add(this.lb_magv_pc);
+            this.Name = "Form_TruongDonVi_ThemPC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_TruongDonVi_ThemPC";
+            this.gb_DSKHMO.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_mact_pc;
+        private System.Windows.Forms.TextBox txt_nam_pc;
+        private System.Windows.Forms.TextBox txt_hocki_pc;
+        private System.Windows.Forms.TextBox txt_mahp_pc;
+        private System.Windows.Forms.Label lb_mact_pc;
+        private System.Windows.Forms.Label lb_nam_pc;
+        private System.Windows.Forms.Label lb_hk_pc;
+        private System.Windows.Forms.Label lb_mahp_pc;
+        private System.Windows.Forms.Label lb_magv_pc;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox gb_DSKHMO;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lb_phancong;
+        private System.Windows.Forms.Button btn_QuayLaiPC;
+        private System.Windows.Forms.Button btn_ThemPC;
+    }
+}
---
+++ 21127331_21127388_21127537_21127695/21127331_21127388_21127537_21127695/Form_TruongDonVi_ThemPC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21127331_21127388_21127537_21127695
+{
+    public partial class Form_TruongDonVi_ThemPC : Form
+    {
+        public Form_TruongDonVi_ThemPC()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_QuayLaiPC_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_ThemPC_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
