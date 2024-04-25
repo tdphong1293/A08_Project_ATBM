6a1b7dfe71b1c522815e414950b285d4a5bda321
Tomteohehe <hoangtranthong03@email.com>
2024-04-25 20:36:13 +0700

    update them chinh sach phan cong de lay MAGV cho dangky

--- ScriptSQL/Project_PH2_YC1.sql
+++ ScriptSQL/Project_PH2_YC1.sql
@@ -574,3 +574,54 @@ end;
 /
 
 grant select on HOCPHAN to rl_SinhVien;
+
+create or replace function pc6_SinhVien_PHANCONG
+    (p_schema varchar2, p_obj varchar2)
+return varchar2
+as
+    cursor cur1 is (
+        select granted_role
+        from DBA_ROLE_PRIVS
+        where grantee = sys_context('userenv', 'session_user')
+    );
+    cursor cur2 is (
+        select MACT
+        from SINHVIEN
+    );
+    v_mact varchar2(10);
+    type role_tab is table of varchar2(100);
+    roles role_tab := role_tab();
+begin
+    if (sys_context('userenv', 'session_user') = 'OLS_ADMIN') then
+        return '';
+    end if; 
+
+    for res in cur1 loop
+        roles.extend;
+        roles(roles.count) := res.granted_role;
+    end loop;
+    
+    if 'RL_SINHVIEN' member of roles then
+        open cur2;
+        fetch cur2 into v_mact;
+        close cur2;
+        return 'MACT = ' || '''' || v_mact || '''';   
+    else 
+        return '';
+    end if;   
+end;
+/
+
+begin
+    dbms_rls.add_policy(
+        object_schema => 'OLS_ADMIN',
+        object_name => 'PHANCONG',
+        policy_name => 'pc6',
+        function_schema => 'OLS_ADMIN',
+        policy_function => 'pc6_SinhVien_PHANCONG',
+        statement_types => 'select'
+    );
+end;
+/
+
+grant select on PHANCONG to rl_SinhVien;
