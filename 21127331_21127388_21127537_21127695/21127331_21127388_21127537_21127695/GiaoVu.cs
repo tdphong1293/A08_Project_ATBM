72363c2e76492dfd26308095a1046541b58a7ee9
Tomteohehe <hoangtranthong03@email.com>
2024-04-27 23:31:54 +0700

    update CS3 dang ky

--- 21127331_21127388_21127537_21127695/21127331_21127388_21127537_21127695/GiaoVu.cs
+++ 21127331_21127388_21127537_21127695/21127331_21127388_21127537_21127695/GiaoVu.cs
@@ -20,6 +20,8 @@ namespace _21127331_21127388_21127537_21127695
         private Timer searchhp;
         private Timer searchkhmo;
         private Timer searchphancong;
+        private Timer searchdk;
+        public static String mact;
         public GiaoVu()
         {
             InitializeComponent();
@@ -57,6 +59,13 @@ namespace _21127331_21127388_21127537_21127695
             searchphancong.Tick += searchphancong_Tick;
             SearchAndFill_PhanCong("", "", "");
             chinhsua_pc.Enabled = false;
+
+
+            dtgdk.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            searchdk = new Timer();
+            searchdk.Interval = 1000;
+            searchdk.Tick += searchdk_Tick;
+            SearchAndFill_DangKy("");
         }
 
         private void ten_giaovu()
@@ -134,6 +143,7 @@ namespace _21127331_21127388_21127537_21127695
                 txt_dtbtl_sv.Text = float.Parse(row.Cells["DTBTL"].Value.ToString()).ToString("F2");
                 txt_diachi_sv.Text = row.Cells["DCHI"].Value.ToString();
                 txt_mact_sv.Text = row.Cells["MACT"].Value.ToString();
+                mact = row.Cells["MACT"].Value.ToString();
                 txt_manganh_sv.Text = row.Cells["MANGANH"].Value.ToString();
             }
             else
@@ -459,6 +469,83 @@ namespace _21127331_21127388_21127537_21127695
             }
         }
 
+        private void dtgdk_masv_TextChanged(object sender, EventArgs e)
+        {
+            searchdk.Stop();
+            searchdk.Start();
+        }
+
+        private void searchdk_Tick(object sender, EventArgs e)
+        {
+            searchdk.Stop();
+            SearchAndFill_DangKy(dtgdk_masv.Text);
+        }
+
+        private void dtgdk_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dtgdk.Rows[e.RowIndex];
+                txt_dk_masv.Text = row.Cells["MASV"].Value.ToString();
+                txt_dk_magv.Text = row.Cells["MAGV"].Value.ToString();
+                txt_dk_mahp.Text = row.Cells["MAHP"].Value.ToString();
+                txt_dk_hki.Text = row.Cells["HK"].Value.ToString();
+                txt_dk_nam.Text = row.Cells["NAM"].Value.ToString();
+                txt_dk_mact.Text = row.Cells["MACT"].Value.ToString();
+            }
+            else
+            {
+                txt_dk_masv.Text = "";
+                txt_dk_magv.Text = "";
+                txt_dk_mahp.Text = "";
+                txt_dk_hki.Text = "";
+                txt_dk_nam.Text = "";
+                txt_dk_mact.Text = "";
+            }
+        }
+
+        private void SearchAndFill_DangKy(string masv)
+        {
+            int thang = DateTime.Now.Month;
+            int hki;
+            if (1 <= thang && thang <= 4) hki = 1;
+            else if (5 <= thang && thang <= 8) hki = 2;
+            else hki = 3;
+            try
+            {
+                string query = $"select * from OLS_ADMIN.uv_GiaoVu_DANGKY where HK={hki}";
+
+                if (!string.IsNullOrEmpty(masv) && masv.Trim() != "")
+                {
+                    query += " AND MASV = :masv";
+                }
+
+                using (OracleCommand cmd = new OracleCommand(query, conn))
+                {
+                    if (!string.IsNullOrEmpty(masv) && masv.Trim() != "")
+                    {
+                        cmd.Parameters.Add(":masv", masv);
+                    }
+
+                    using (OracleDataReader reader = cmd.ExecuteReader())
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        dtgdk.DataSource = dataTable;
+                        dtgdk.Columns["DIEMTH"].Visible = false;
+                        dtgdk.Columns["DIEMQT"].Visible = false;
+                        dtgdk.Columns["DIEMCK"].Visible = false;
+                        dtgdk.Columns["DIEMTK"].Visible = false;
+
+                    }
+                }
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             conn.Close();
@@ -545,6 +632,13 @@ namespace _21127331_21127388_21127537_21127695
             public string Pc_nam { get; set; }
             public string Pc_mact { get; set; }
             public string Pc_oldmagv { get; set; }
+
+            public string Dk_masv { get; set; }
+            public string Dk_magv { get; set; }
+            public string Dk_mahp { get; set; }
+            public string Dk_hki { get; set; }
+            public string Dk_nam { get; set; }
+            public string Dk_mact { get; set; }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -648,7 +742,36 @@ namespace _21127331_21127388_21127537_21127695
 
         private void button12_Click(object sender, EventArgs e)
         {
+            ThemDangKy_GiaoVu themdkgv = new ThemDangKy_GiaoVu();
+            DataToPass data = new DataToPass();
+            data.Masv = txt_masv.Text;
+            data.Mact = txt_mact_sv.Text;
 
+            themdkgv.SetData(data);
+            themdkgv.ShowDialog();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            int hki = Int32.Parse(txt_dk_hki.Text);
+            int nam = Int32.Parse(txt_dk_nam.Text);
+            string query = $"delete from OLS_ADMIN.uv_GiaoVu_DANGKY where MASV= '{txt_dk_masv.Text}' and MAGV= '{txt_dk_magv.Text}' and MAHP= '{txt_dk_mahp.Text}' " +
+                $"and HK= {hki} and nam= {nam} and MACT = '{txt_dk_mact.Text}' and DIEMTH is null and DIEMQT is null and DIEMCK is null and DIEMTK is null";
+            using (OracleCommand cmd = new OracleCommand(query, conn))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Xóa đăng ký thành công");
+                    SearchAndFill_DangKy("");
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
         }
     }
 }
---
+++ 21127331_21127388_21127537_21127695/21127331_21127388_21127537_21127695/ThemDangKy_GiaoVu.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace _21127331_21127388_21127537_21127695
+{
+    partial class ThemDangKy_GiaoVu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgdk = new System.Windows.Forms.DataGridView();
+            this.label20 = new System.Windows.Forms.Label();
+            this.dtgdk_mahp = new System.Windows.Forms.TextBox();
+            this.txt_dk_mact = new System.Windows.Forms.TextBox();
+            this.txt_dk_nam = new System.Windows.Forms.TextBox();
+            this.txt_dk_mahp = new System.Windows.Forms.TextBox();
+            this.txt_dk_hki = new System.Windows.Forms.TextBox();
+            this.txt_dk_masv = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label32 = new System.Windows.Forms.Label();
+            this.txt_dk_magv = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgdk)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgdk
+            // 
+            this.dtgdk.AllowUserToDeleteRows = false;
+            this.dtgdk.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgdk.Location = new System.Drawing.Point(23, 140);
+            this.dtgdk.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtgdk.Name = "dtgdk";
+            this.dtgdk.RowHeadersWidth = 62;
+            this.dtgdk.Size = new System.Drawing.Size(1016, 537);
+            this.dtgdk.TabIndex = 57;
+            this.dtgdk.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgdk_CellClick);
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(46, 74);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(101, 20);
+            this.label20.TabIndex = 65;
+            this.label20.Text = "Mã học phần";
+            // 
+            // dtgdk_mahp
+            // 
+            this.dtgdk_mahp.Location = new System.Drawing.Point(172, 71);
+            this.dtgdk_mahp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtgdk_mahp.Name = "dtgdk_mahp";
+            this.dtgdk_mahp.Size = new System.Drawing.Size(164, 26);
+            this.dtgdk_mahp.TabIndex = 64;
+            this.dtgdk_mahp.TextChanged += new System.EventHandler(this.dtgdk_mahp_TextChanged);
+            // 
+            // txt_dk_mact
+            // 
+            this.txt_dk_mact.Enabled = false;
+            this.txt_dk_mact.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dk_mact.Location = new System.Drawing.Point(1330, 556);
+            this.txt_dk_mact.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_dk_mact.Name = "txt_dk_mact";
+            this.txt_dk_mact.ReadOnly = true;
+            this.txt_dk_mact.Size = new System.Drawing.Size(462, 37);
+            this.txt_dk_mact.TabIndex = 75;
+            this.txt_dk_mact.TabStop = false;
+            // 
+            // txt_dk_nam
+            // 
+            this.txt_dk_nam.Enabled = false;
+            this.txt_dk_nam.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dk_nam.Location = new System.Drawing.Point(1330, 474);
+            this.txt_dk_nam.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_dk_nam.Name = "txt_dk_nam";
+            this.txt_dk_nam.ReadOnly = true;
+            this.txt_dk_nam.Size = new System.Drawing.Size(462, 37);
+            this.txt_dk_nam.TabIndex = 74;
+            this.txt_dk_nam.TabStop = false;
+            // 
+            // txt_dk_mahp
+            // 
+            this.txt_dk_mahp.Enabled = false;
+            this.txt_dk_mahp.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dk_mahp.Location = new System.Drawing.Point(1330, 301);
+            this.txt_dk_mahp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_dk_mahp.Name = "txt_dk_mahp";
+            this.txt_dk_mahp.ReadOnly = true;
+            this.txt_dk_mahp.Size = new System.Drawing.Size(462, 37);
+            this.txt_dk_mahp.TabIndex = 72;
+            this.txt_dk_mahp.TabStop = false;
+            // 
+            // txt_dk_hki
+            // 
+            this.txt_dk_hki.Enabled = false;
+            this.txt_dk_hki.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dk_hki.Location = new System.Drawing.Point(1330, 387);
+            this.txt_dk_hki.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_dk_hki.Name = "txt_dk_hki";
+            this.txt_dk_hki.ReadOnly = true;
+            this.txt_dk_hki.Size = new System.Drawing.Size(462, 37);
+            this.txt_dk_hki.TabIndex = 73;
+            this.txt_dk_hki.TabStop = false;
+            // 
+            // txt_dk_masv
+            // 
+            this.txt_dk_masv.Enabled = false;
+            this.txt_dk_masv.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dk_masv.Location = new System.Drawing.Point(1330, 210);
+            this.txt_dk_masv.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_dk_masv.Name = "txt_dk_masv";
+            this.txt_dk_masv.ReadOnly = true;
+            this.txt_dk_masv.Size = new System.Drawing.Size(462, 37);
+            this.txt_dk_masv.TabIndex = 71;
+            this.txt_dk_masv.TabStop = false;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(1060, 559);
+            this.label17.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(186, 31);
+            this.label17.TabIndex = 70;
+            this.label17.Text = "Mã chương trình";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(1060, 304);
+            this.label18.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(148, 31);
+            this.label18.TabIndex = 67;
+            this.label18.Text = "Mã học phần";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(1060, 477);
+            this.label19.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(63, 31);
+            this.label19.TabIndex = 69;
+            this.label19.Text = "Năm";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(1060, 390);
+            this.label21.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(77, 31);
+            this.label21.TabIndex = 68;
+            this.label21.Text = "Học kì";
+            // 
+            // label32
+            // 
+            this.label32.AutoSize = true;
+            this.label32.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label32.Location = new System.Drawing.Point(1060, 213);
+            this.label32.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label32.Name = "label32";
+            this.label32.Size = new System.Drawing.Size(143, 31);
+            this.label32.TabIndex = 66;
+            this.label32.Text = "Mã sinh viên";
+            // 
+            // txt_dk_magv
+            // 
+            this.txt_dk_magv.Enabled = false;
+            this.txt_dk_magv.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dk_magv.Location = new System.Drawing.Point(1330, 640);
+            this.txt_dk_magv.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_dk_magv.Name = "txt_dk_magv";
+            this.txt_dk_magv.ReadOnly = true;
+            this.txt_dk_magv.Size = new System.Drawing.Size(462, 37);
+            this.txt_dk_magv.TabIndex = 76;
+            this.txt_dk_magv.TabStop = false;
+            this.txt_dk_magv.Visible = false;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(839, 66);
+            this.button12.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(200, 36);
+            this.button12.TabIndex = 77;
+            this.button12.Text = "Thêm ĐĂNG KÝ";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // ThemDangKy_GiaoVu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1809, 734);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.txt_dk_magv);
+            this.Controls.Add(this.txt_dk_mact);
+            this.Controls.Add(this.txt_dk_nam);
+            this.Controls.Add(this.txt_dk_mahp);
+            this.Controls.Add(this.txt_dk_hki);
+            this.Controls.Add(this.txt_dk_masv);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label32);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.dtgdk_mahp);
+            this.Controls.Add(this.dtgdk);
+            this.Name = "ThemDangKy_GiaoVu";
+            this.Text = "ThemDangKy_GiaoVu";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgdk)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dtgdk;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox dtgdk_mahp;
+        private System.Windows.Forms.TextBox txt_dk_mact;
+        private System.Windows.Forms.TextBox txt_dk_nam;
+        private System.Windows.Forms.TextBox txt_dk_mahp;
+        private System.Windows.Forms.TextBox txt_dk_hki;
+        private System.Windows.Forms.TextBox txt_dk_masv;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label32;
+        private System.Windows.Forms.TextBox txt_dk_magv;
+        private System.Windows.Forms.Button button12;
+    }
+}
